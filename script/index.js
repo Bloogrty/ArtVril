2b69e369f2c1e902da7916941d612110abb98c6b
Cristanto <cristanto.sidharta@gmail.com>
2023-05-27 01:02:14 +0700

    add button and js on commission page

--- script/index.js
+++ script/index.js
@@ -2,3 +2,15 @@ function toggleSidebar() {
     var sidebar = document.querySelector('.sidebar');
     sidebar.classList.toggle('open');
 }
+
+function validateForm() {
+    var checkbox = document.getElementById("agree");
+    var errorMsg = document.getElementById("error");
+
+    if (checkbox.checked == false) {
+        errorMsg.textContent = "Please agree to the terms and conditions";
+        return false;
+    } 
+    errorMsg.textContent = "";
+    return true;
+}
