63b0cedcc92aab009f0d04b5d57f12d0f1fdd35d
Skullition <78258797+Skullition@users.noreply.github.com>
2023-05-30 15:27:11 +0700

    Load navbar page into form-commission

--- script/form-commission.js
+++ script/form-commission.js
@@ -1,10 +1,3 @@
-function validateForm(form) {
-    if (form.title === "esad") {
-        alert("Title must be filled out!");
-        return false;
-    } else if (form.description === "") {
-        alert("Description must be filled out!");
-        return false;
-    }
-    return true;
+function loadNavbar() {
+    $("nav").load("../component/navbar.html")
 }
