25a8b4aa300c1c061a3fdba0771caff300941759
Skullition <78258797+Skullition@users.noreply.github.com>
2023-06-07 12:43:54 +0700

    Refactor all pages to call headerFooter instead of duplicating pages

---
+++ script/headerFooter.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.nav').load('../component/navbar.html')
+});
+$(document).ready(function () {
+    $('.footer').load('../component/footer.html')
+});
--- script/index.js
+++ script/index.js
@@ -1,16 +1,15 @@
 function toggleSidebar() {
-    var sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('open');
+    $('.sidebar').toggleClass('open');
 }
 
 function validateForm() {
-    var checkbox = document.getElementById("agree");
-    var errorMsg = document.getElementById("error");
+    let checkbox = document.getElementById("agree");
+    let errorMsg = document.getElementById("error");
 
-    if (checkbox.checked == false) {
+    if (checkbox.checked === false) {
         errorMsg.textContent = "Please agree to the terms and conditions";
         return false;
-    } 
+    }
     errorMsg.textContent = "";
     return true;
 }
