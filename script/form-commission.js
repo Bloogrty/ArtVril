3cfb287630a3c8a9a4661872e9893b0be9a2f28d
Skullition <78258797+Skullition@users.noreply.github.com>
2023-06-07 10:53:53 +0700

    Properly redirect user to form-commission-confirmation when inputs are added correctly

--- script/form-commission.js
+++ script/form-commission.js
@@ -11,40 +11,47 @@ function validateForm(event) {
     contactNumberError.textContent = '';
     artworkError.textContent = '';
     finishDateError.textContent = '';
-  
+
     const fullName = document.getElementById('full-name').value;
     const email = document.getElementById('email').value;
     const contactNumber = document.getElementById('contact-number').value;
     const artwork = document.getElementById('artwork').value;
     const finishDate = document.getElementById('finish-date').value;
-  
+
     if (fullName.trim() === '') {
       fullNameError.textContent = 'Please enter your full name.';
+      return;
     }
-  
+
 
     if (email.trim() === '') {
       emailError.textContent = 'Please enter your email.';
+        return;
     } else if (!validateEmail(email)) {
       emailError.textContent = 'Please enter a valid email address.';
+        return;
     }
-  
+
 
     if (contactNumber.trim() === '') {
       contactNumberError.textContent = 'Please enter your contact number.';
+        return;
     }
-  
+
 
     if (artwork === '') {
       artworkError.textContent = 'Please select the artwork type.';
+        return;
     }
-  
+
 
     if (finishDate === '') {
       finishDateError.textContent = 'Please select the finish date.';
+        return;
     }
+    window.location.replace("../view/form-commission-confirmation.html");
   }
-  
+
 
   function validateEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
